d9b27667599c6effb8f333d17fa2d2d1f92204ea
Mandersen2016 <mandersen4@my.devry.edu>
2025-11-10 13:42:46 -0500

    refactor AdzunaClient and Client classes to improve API request handling

--- src/main/java/JobSearch/Clients/AdzunaClient.java
+++ src/main/java/JobSearch/Clients/AdzunaClient.java
@@ -2,11 +2,41 @@ package JobSearch.Clients;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 public class AdzunaClient extends Client {
+    private final RestTemplate restTemplate;
+    private URI uri;
+    private final String baseUrl;
+    private final String apiKey;
+    private final String apiId;
+
+
+    public AdzunaClient(RestTemplate restTemplate, URI uri, String baseUrl, String apiKey, String apiId) {
+        this.restTemplate = restTemplate;
+        this.uri = uri;
+        this.baseUrl = baseUrl;
+        this.apiKey = apiKey;
+        this.apiId = apiId;
+    }
+
     @Override
-    public void client(RestTemplate restTemplate, String url, String apiKey) {
+    public URI client(RestTemplate restTemplate, String url, String apiKey) {
+        uri = UriComponentsBuilder.fromUri(URI.create("http://api.adzuna.com"))
+                .path("/v1/api/jobs/gb/search/1?")
+                .queryParam("app_id", apiId)
+                .queryParam("app_key", apiKey)
+                .queryParam("results_per_page", 20)
+                .queryParam("what","barista")
+                .queryParam("where", "Raleigh").queryParam("full-time", 1)
+                .queryParam("what-excluded","")
+                .queryParam("content-type", "application/json")
+                .build()
+                .toUri();
         logRequest("AdzunaClient", url);
+        return uri;
     }
 
     @Override
--- src/main/java/JobSearch/Clients/Client.java
+++ src/main/java/JobSearch/Clients/Client.java
@@ -4,16 +4,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
+
 public abstract class Client {
 
-    public abstract void client(RestTemplate restTemplate,
-                                final String url,
-                                final String apiKey);
+    public abstract URI client(RestTemplate restTemplate,
+                               final String url,
+                               final String apiKey);
 
     public abstract ResponseEntity<String> getResponseEntity(String baseUrl);
 
     public void logRequest(String apiName, String query) {
         System.out.print("calling: "+ apiName + "Api for the query: " + query);
     }
+
+
 
 }
