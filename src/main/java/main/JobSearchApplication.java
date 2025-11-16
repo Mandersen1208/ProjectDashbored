e4f84a34642d82b48c835807f5347072d065863c
Mandersen2016 <mandersen4@my.devry.edu>
2025-11-16 12:02:33 -0500

    update docker-compose configuration for PostgreSQL with healthcheck and network settings

--- src/main/java/main/JobSearchApplication.java
+++ src/main/java/main/JobSearchApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
@@ -18,7 +17,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @ComponentScan(basePackages = {"JobSearch", "DbConnections", "DashBoardBackend"})
 @EnableJpaRepositories(basePackages = "DbConnections.Repositories")
 @EntityScan(basePackages = "DbConnections.DTO.Entities")
-@EnableRetry
+
 @EnableTransactionManagement
 public class JobSearchApplication {
 
